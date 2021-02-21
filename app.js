d122cde8c2a749d08780fbd629c964209ea0f893
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-21 11:14:58 -0800

    user login

--- app.js
+++ app.js
@@ -5,19 +5,28 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const User = require('./models/User');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-
-  app.get("/", (req, res) => {
-    res.send("Hello Hi")
-  });
-  
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "jim",
+    email: "jim@jim.com",
+    password: "jimisgreat123"
+  })
+  user.save()
+  res.send("Hello Hi")
+});
+  
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
